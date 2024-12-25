8cc835e48c9672144acdd8253ba75e954b6a3986
Dream Journey <mdimrannazirdevloper@gmail.com>
2024-12-25 22:39:33 +0600

    Website

--- docs/main.js
+++ docs/main.js
@@ -1,15 +1,14 @@
-const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-const targetUrl = 'https://www.pexels.com/en-us/api/v3/sponsored-media/photos/car?number=4&page=1';
-
-fetch(proxyUrl + targetUrl, {
+// Fetch data from the Pexels API using the GET request with headers
+fetch('https://unsplash.com/napi/search/photos?orientation=portrait&page=2&per_page=20&query=anime', {
     method: 'GET',
     headers: {
         'Content-Type': 'application/json',
-        'secret-key': 'H2jk9uKnhRmL6WPwh89zBezWvr', // Make sure your key is valid
+        'secret-key': 'H2jk9uKnhRmL6WPwh89zBezWvr'
     }
 })
-    .then(response => response.json())
+    .then(response => response.json()) // Parse the response to JSON
     .then(data => {
+        // Display the raw data inside the 'data' div
         document.getElementById('data').textContent = JSON.stringify(data, null, 2);
     })
     .catch(error => {
